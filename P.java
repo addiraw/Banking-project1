62d8735c3e7bc63766dfb2a9323a58a1661d3828
Addiraw <46880800+addiraw@users.noreply.github.com>
2026-09-07 22:40:00 +0530

    tick tock

--- P.java
+++ P.java
@@ -1,3 +1,57 @@
-public class P {
+class Q {
     
+    boolean isTick = true;
+    boolean isTock = false;
+
+    public synchronized void tick(){
+  
+        while(!isTick){
+            try{
+                System.err.println("Waiting tick");
+                wait();
+            } catch(InterruptedException exception){
+                System.out.println("System interrupted");
+            }
+        }
+        System.err.println("tick");
+        isTick = false;
+        isTock = true;
+        notify();
+
+
+    }
+
+    public synchronized void tock(){
+        while(!isTock){
+            try {
+                System.err.println("Waiting tock");
+
+                wait();
+            } catch (InterruptedException e) {
+                System.err.println("Exception occur");
+            }
+        }
+        System.err.println("tock");
+        isTock = false;
+        isTick = true;
+        notify();
+    }
+}
+public class P{
+    
+    public static void main(String args[]){
+        Q q = new Q();
+        Thread tickThread = new Thread(()->{
+            for(int i=0;i<5;i++)
+            q.tick();
+        },"Tick");
+
+        Thread tockThread = new Thread(()->{
+            for(int i=0;i<5;i++)
+            q.tock();
+        });
+
+        tickThread.start();
+        tockThread.start();
+        }
 }
